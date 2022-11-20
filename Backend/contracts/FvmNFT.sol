bac241d61fa39f76a0cf2fba522cf7080f810439
AAYUSH-GUPTA-coder <60979345+AAYUSH-GUPTA-coder@users.noreply.github.com>
2022-11-20 17:29:42 +0530

    Update FvmNFT.sol

--- Backend/contracts/FvmNFT.sol
+++ Backend/contracts/FvmNFT.sol
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract FvmNFT is ERC721, ERC721URIStorage {
+contract FvmNFT is ERC721URIStorage {
     constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
 
     function mint(address _to, uint _number, string memory _tokenURI) public {
@@ -15,18 +14,5 @@ contract FvmNFT is ERC721, ERC721URIStorage {
         // setting default token Metadata 
         _setTokenURI(i, _tokenURI);
         }
-    }
-
-    // override
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
-        super._burn(tokenId);
-    }
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
     }
 }
